b2bc485240953d38aa91a3adc7e93094f4605068
ganeshrevadi <ganeshrevadi16@gmail.com>
2026-07-01 21:41:08 +0530

    types defination

---
+++ agent/types.go
@@ -0,0 +1,26 @@
+package agent
+
+type Message struct {
+	Role       string     `json:"role"`
+	Content    string     `json:"content"`
+	ToolCalls  []ToolCall `json:"tool_calls,omitempty"`
+	ToolCallID string     `json:"tool_call_id,omitempty"`
+	ToolName   string     `json:"-"` // agent internal use only
+}
+
+type ToolCall struct {
+	ID       string `json:"id"`
+	Type     string `json:"type"`
+	Function struct {
+		Name      string `json:"name"`
+		Arguments string `json:"arguments"`
+	} `json:"function"`
+}
+
+type StreamEvent struct {
+	Type         string
+	Content      string
+	ToolCall     *ToolCall
+	FinishReason string
+	Error        error
+}
